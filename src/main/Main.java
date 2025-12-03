b76a59dde12e705cc4219aa080ec79a210b10978
Ricardo Ferreira da Silva <dasilvaricardo787@gmail.com>
2025-12-02 21:22:37 -0300

    fix: Adiciona verificação de segurança 'args.length' na Main para evitar ArrayIndexOutOfBounds

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,10 @@ import ui.Menu;
 
 public class Main {
     public static void main(String[] args) {
+        
+        if (args == null || args.length == 0) {
+        } 
+        
         Menu menu = new Menu();
         SistemaLancheria sistema = new SistemaLancheria(menu);
         sistema.iniciar();
